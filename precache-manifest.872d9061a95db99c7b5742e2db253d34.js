670d99d8deec8b34436c2057a321590e1b21383e
Lauren McCarthy <laurenleemccarthy@gmail.com>
2020-05-29 00:22:06 -0700

    Updates

---
+++ precache-manifest.872d9061a95db99c7b5742e2db253d34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb3cab45ea1493a958059ec6e9c9601c",
+    "url": "/p5.js-1.0-contributors-zine/index.html"
+  },
+  {
+    "revision": "a04908049f24b7d885f5",
+    "url": "/p5.js-1.0-contributors-zine/static/css/2.98bc8785.chunk.css"
+  },
+  {
+    "revision": "bc1d1efbcf3f19043074",
+    "url": "/p5.js-1.0-contributors-zine/static/css/main.f3ff8dc3.chunk.css"
+  },
+  {
+    "revision": "a04908049f24b7d885f5",
+    "url": "/p5.js-1.0-contributors-zine/static/js/2.b7aa9912.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/p5.js-1.0-contributors-zine/static/js/2.b7aa9912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1d1efbcf3f19043074",
+    "url": "/p5.js-1.0-contributors-zine/static/js/main.f4eba936.chunk.js"
+  },
+  {
+    "revision": "25db0effd6fa44a99822",
+    "url": "/p5.js-1.0-contributors-zine/static/js/runtime-main.01b5d212.js"
+  }
+]);
