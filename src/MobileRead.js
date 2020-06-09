d5363d796d16ca4085fb1b9d87ba62bdc24fc083
Lauren McCarthy <laurenleemccarthy@gmail.com>
2020-06-08 19:06:40 -0700

    d

--- src/MobileRead.js
+++ src/MobileRead.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Container, Col } from 'reactstrap';
-import Skew from './Skew.js';
 
 class MobileRead extends Component {	 
 	render() {
@@ -9,7 +8,7 @@ class MobileRead extends Component {
 			<Container className='aboutText d-block d-sm-none'>
 				<h1 id='read-heading' className='sr-only'>Read</h1>
 				<Col xs={12} sm={12}>
-					<p><Skew text='This zine is so good we want you to read it at full size! Please open this on a desktop or tablet.'/></p>
+					<p>This zine is so good we want you to read it at full size! Please open this on a desktop or tablet.</p>
 				</Col>
 			</Container>
 			</main>
