8a69a3b6b5ef72a816f7eec44942b3c4fc6cb3a8
Lauren McCarthy <laurenleemccarthy@gmail.com>
2020-06-16 23:55:17 -0700

    Updates

--- precache-manifest.540528f808281f6fdc5edec61fccc7a8.js
+++ precache-manifest.540528f808281f6fdc5edec61fccc7a8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eda00d26f3f1d79030ae7552ab489615",
+    "revision": "eb228759305628fc309501367fdfb9a4",
     "url": "/p5.js-1.0-contributors-zine/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/p5.js-1.0-contributors-zine/static/css/2.98bc8785.chunk.css"
   },
   {
-    "revision": "62457065289d78dc5ad4",
-    "url": "/p5.js-1.0-contributors-zine/static/css/main.d707d850.chunk.css"
+    "revision": "8afc59df3ee0d655ef3d",
+    "url": "/p5.js-1.0-contributors-zine/static/css/main.e5cbfd22.chunk.css"
   },
   {
     "revision": "18125f2505d2ca8b5a09",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/p5.js-1.0-contributors-zine/static/js/2.5bef6517.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "62457065289d78dc5ad4",
-    "url": "/p5.js-1.0-contributors-zine/static/js/main.ac8ab001.chunk.js"
+    "revision": "8afc59df3ee0d655ef3d",
+    "url": "/p5.js-1.0-contributors-zine/static/js/main.1591a3cb.chunk.js"
   },
   {
     "revision": "25db0effd6fa44a99822",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/p5.js-1.0-contributors-zine/precache-manifest.5ae742a2b7485738676c6b266a10ec5c.js"
+  "/p5.js-1.0-contributors-zine/precache-manifest.540528f808281f6fdc5edec61fccc7a8.js"
 );
 
 self.addEventListener('message', (event) => {
