c5f354719a899d4a8a5b0aa6ad574a0cdee28643
Lauren McCarthy <laurenleemccarthy@gmail.com>
2020-06-19 08:39:20 -0700

    ed

--- src/MobileRead.js
+++ src/MobileRead.js
@@ -8,7 +8,7 @@ class MobileRead extends Component {
 			<Container className='aboutText d-block d-sm-none'>
 				<h1 id='read-heading' className='sr-only'>Read</h1>
 				<Col xs={12} sm={12}>
-					<p>This zine is so good we want you to read it at full size! Please open this on a desktop or tablet.</p>
+					<p>Because this zine is so special to us, we ask that you read it at full size! Please open this link on a desktop or tablet.</p>
 				</Col>
 			</Container>
 			</main>
